fdf4d0817bf06ac7606ef0afcf0146d099c28461
Tyler Lafayette <tylerlafayette@gmail.com>
2019-03-23 12:43:14 -0700

    Fixed router and added Navbar component

---
+++ Auta/app/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import { View, Text } from "react-native"
+
+import styles from "./styles"
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
