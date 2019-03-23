4646fff2caed5dc13f67f0acba91efe0002095a6
Tyler Lafayette <tylerlafayette@gmail.com>
2019-03-23 12:13:49 -0700

    Start React Native UI

---
+++ Auta/app/app.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import { AsyncStorage, View, Text, StatusBar, ActivityIndicator } from "react-native"
+import { withRouter, NativeRouter, Switch, Route, AndroidBackButton, Alert, Link } from "react-router-native"
+
+class App extends Component {
+    constructor() {
+        super()
+        console.disableYellowBox = true;
+        this.state = {
+
+        }
+    }
+    componentDidMount = async () => {
+
+    }
+    render() {
+        return (
+            <View style={{
+                backgroundColor: "#FFFFFF",
+                flex: 1
+            }}>
+                <StatusBar backgroundColor={"#FFFFFF"} barStyle={"dark-content"} />
+                <AndroidBackButton>
+                    <Switch>
+                        <Route exact path="/" component={() => <Text>Hello</Text>} />
+                    </Switch>
+                </AndroidBackButton>
+            </View>
+        )
+    }
+}
+
+App = withRouter(App)
+
+export default class Index extends Component {
+    render() {
+        return (
+            <NativeRouter>
+                <App></App>
+            </NativeRouter>
+        )
+    }
+}
---
+++ Auta/app/styles/common.js
@@ -0,0 +1,4 @@
+export const PRIMARY_BG = "#FFFFFF"
+export const PRIMARY_TEXT = "#0E2C45"
+export const DANGER_BTN_BG = "#FFC8C8"
+export const DANGER_BTN_TEXT = "#B72F2F"
