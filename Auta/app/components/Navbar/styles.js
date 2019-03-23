4d1164b3801602b398ef60bc58184fc309d151fc
Tyler Lafayette <tylerlafayette@gmail.com>
2019-03-23 14:19:55 -0700

    Fix parameters in ClassifierModule

--- Auta/app/components/Navbar/styles.js
+++ Auta/app/components/Navbar/styles.js
@@ -1,5 +1,22 @@
 import { StyleSheet } from "react-native"
 
+import { PRIMARY_TEXT } from "../../styles/common"
+
 export default StyleSheet.create({
-    
+    navbarWrapper: {
+        padding: 30,
+        flexDirection: "row",
+        height: 100,
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    title: {
+        fontSize: 12,
+        fontWeight: "bold",
+        letterSpacing: 2,
+        color: PRIMARY_TEXT
+    },
+    placeholder: {
+        width: 28
+    }
 })
