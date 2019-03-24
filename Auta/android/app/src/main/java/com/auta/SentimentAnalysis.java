811ae2ad87c8d5b23dd86e932470cee20c7d2537
Kevin Chu <kevinlc221@gmail.com>
2019-03-23 21:49:15 -0700

    mong

--- Auta/android/app/src/main/java/com/auta/SentimentAnalysis.java
+++ Auta/android/app/src/main/java/com/auta/SentimentAnalysis.java
@@ -1,7 +1,11 @@
 package com.auta;
 
+import com.auta.sentiment.SentimentAnalyzer;
+import com.auta.sentiment.SentimentResult;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class SentimentAnalysis extends ReactContextBaseJavaModule {
 
@@ -12,5 +16,18 @@ public class SentimentAnalysis extends ReactContextBaseJavaModule {
     @Override
     public String getName(){
         return "Sentiment";
+    }
+
+    /*
+    * "very negative" = 0 "Negative" = 1 "Neutral" = 2 "Positive" = 3
+    * "very positive" = 4
+     */
+    @ReactMethod
+    public void analyze(String sentence, Callback errorCallback, Callback successCallback){
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+        sentimentAnalyzer.initialize();
+        SentimentResult sentimentResult = sentimentAnalyzer.getSentimentResult(sentence);
+
+        successCallback.invoke((int) sentimentResult.getSentimentScore());
     }
 }
---
+++ Auta/android/app/src/main/java/com/auta/sentiment/SentimentResult.java
@@ -0,0 +1,37 @@
+package com.auta.sentiment;
+
+public class SentimentResult {
+
+    private double sentimentScore;
+    private String sentimentType;
+    private SentimentClassification sentimentClass;
+
+    public double getSentiment() {
+        return sentimentScore;
+    }
+
+    public double getSentimentScore() {
+        return sentimentScore;
+    }
+
+    public void setSentimentScore(double sentimentScore) {
+        this.sentimentScore = sentimentScore;
+    }
+
+    public String getSentimentType() {
+        return sentimentType;
+    }
+
+    public void setSentimentType(String sentimentType) {
+        this.sentimentType = sentimentType;
+    }
+
+    public SentimentClassification getSentimentClass() {
+        return sentimentClass;
+    }
+
+    public void setSentimentClass(SentimentClassification sentimentClass) {
+        this.sentimentClass = sentimentClass;
+    }
+
+}
